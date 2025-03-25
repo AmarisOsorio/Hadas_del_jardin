22512afb039b08421dec90c92de174795c93156f
Amaris <amarisosorio10@gmail.com>
2025-03-25 06:03:00 -0600

    intento fallido :(

--- Roseta/src/App.jsx
+++ Roseta/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Input from './components/Input/Input'
 import Button from './components/Button/Button'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -26,6 +27,8 @@ function App() {
       <br />
       <Button />
     </div>
+
+    
       
     </>
   )
